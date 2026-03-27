8c6274c7363d4820861ea8ecaa78ff80f1c12433
flipicets <fellipepse@gmail.com>
2026-03-27 15:40:36 -0300

    Primeira condicional
    Só um modelo do tipo de exercício kkkkkkk affferrr

--- main.py
+++ main.py
@@ -5,10 +5,25 @@ calculo_imc = (peso_pessoa * 10000 ) / (altura_pessoa * altura_pessoa)
 print("Modos de exercício: ")
 print("Para intenso leve 1")
 print("Para moderado insira 2")
-print("Para leve intenso 3")
+print("Para leve insira 3")
 intensidade_exercicio = int(input("Insira o quão intenso gostaria de ser seu exercício: "))
 
 meta_hidratacao = (peso_pessoa * 0.035) + (intensidade_exercicio * 0.5)
+
+print("Qual o tipo de exercício que você irá realizar: ")
+print("Para Aeróbico insira 1")
+print("Para Cardiovascular insira 2")
+print("Para alongamento insira 3")
+tipo_exercicio = int(input("Insira o tipo de exercício: "))
+
+if tipo_exercicio == 1:
+    print()
+elif tipo_exercicio == 2:
+    print()
+elif tipo_exercicio == 3:
+    print()
+else:
+    print()
 
 
 print(calculo_imc)
