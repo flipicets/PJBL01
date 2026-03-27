9b360d46b78c143bf9015bdcffe4b1aa9ab43418
flipicets <fellipepse@gmail.com>
2026-03-27 09:44:38 -0300

    IMC e Hidr.
    Primeiros calculos de imc e de meta de hidratacao

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("oie")
+peso_pessoa = float(input("Insira seu peso em quilogramas: "))
+altura_pessoa = int(input("Insira sua altura em centímetros: "))
+calculo_imc = (peso_pessoa * 10000 ) / (altura_pessoa * altura_pessoa)
+
+print("Modos de exercício: ")
+print("Para intenso leve 1")
+print("Para moderado insira 2")
+print("Para leve intenso 3")
+intensidade_exercicio = int(input("Insira o quão intenso gostaria de ser seu exercício: "))
+
+meta_hidratacao = (peso_pessoa * 0.035) + (intensidade_exercicio * 0.5)
+
+
+print(calculo_imc)
+print(meta_hidratacao)
