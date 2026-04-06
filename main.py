d857d39d934fd2fb03856b6f6f5da63e20dfa837
flipicets <fellipepse@gmail.com>
2026-04-06 12:16:45 -0300

    resumo diario
    adidididididididdioddoidododdiodidodidodido MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 menu_iniciar = 0
-while menu_iniciar != 1 and menu_iniciar != 2 and menu_iniciar != 3 and menu_iniciar != 4 and menu_iniciar != 5 and menu_iniciar != 6:
-    print("Qual funcionalidade deseja acessar")
+calculo_imc = 0
+meta_hidratacao = 0
+calorias_gastas = 0
+
+while menu_iniciar != 6:
+    print("-----FUNCIONALIDADES-----")
     print("Para cálculo de IMC, digite 1")
     print("Para meta diária de hidratação, digite 2")
     print("Para saber seu gasto calórico em atividade física, digite 3")
     print("Para verificar a meta diária de exercício, digite 4")
     print("Para mostrar seu resumo diário, digite 5")
     print("Para sair, digite 6")
-    menu_iniciar = int(input())
-    print()
+    menu_iniciar = int(input("Insira a opção desejada: "))
 
     if menu_iniciar == 1:
         peso_pessoa = float(input("Insira seu peso em quilogramas: "))
@@ -27,18 +30,17 @@ while menu_iniciar != 1 and menu_iniciar != 2 and menu_iniciar != 3 and menu_ini
         print("Para sexo feminino, digite 2")
         sexo_pessoa = int(input())
 
+
         while sexo_pessoa != 1 and sexo_pessoa != 2:
             print("Por favor, escolha uma opção valida.")
             print("Para sexo masculino, digite 1")
             print("Para sexo feminino, digite 2")
             sexo_pessoa = int(input())
 
-
         peso_pessoa = float(input("Insira seu peso em quilogramas: "))
         idade_pessoa = int(input("Insira sua idade em anos: "))
-        altura_pessoa = int(input("Insira sua altura em centímetros:"))
+        altura_pessoa = int(input("Insira sua altura em centímetros: "))
 
-        print()
         print("Modos de exercício: ")
         print("Para leve leve 1")
         print("Para moderado insira 2")
@@ -55,7 +57,6 @@ while menu_iniciar != 1 and menu_iniciar != 2 and menu_iniciar != 3 and menu_ini
             print("Para intenso insira 3")
             intensidade_exercicio = int(input())
 
-        calorias_gastas = 0
         if sexo_pessoa == 1:
             taxa_metabolica = (10 * peso_pessoa) + (6.25 * altura_pessoa) - (5 * idade_pessoa) + 5
             if intensidade_exercicio == 1:
@@ -73,17 +74,26 @@ while menu_iniciar != 1 and menu_iniciar != 2 and menu_iniciar != 3 and menu_ini
                 calorias_gastas = taxa_metabolica * 1.55
             else:
                 calorias_gastas = taxa_metabolica * 1.9
+
         print("Você gastou, em média,", calorias_gastas, "calorias hoje.")
 
     elif menu_iniciar == 4:
         print()
 
     elif menu_iniciar == 5:
+        if calculo_imc != 0 and meta_hidratacao != 0 and calorias_gastas != 0:
+            print("Aqui vai seu resumo diário:")
+            print("Se você seguiu as metas e orientações corretamente, seu IMC é de: ",calculo_imc)
+            print("Você bebeu, em média,", meta_hidratacao, "litros de água.")
+            print("Vocẽ gastou, em média,", calorias_gastas, "calorias.")
+        else:
+            print("Você não acessou informações suficientes para obter um resumo diário.")
         print()
 
     elif menu_iniciar == 6:
         break
 
     else:
+        print()
         print("Por favor, insira uma opção valida.")
         print()
